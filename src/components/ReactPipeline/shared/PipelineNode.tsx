fb4e9d291b346928deec404f01a3a46f53371c6b
jamesgiu <james.giuffrida@gmail.com>
2023-11-06 11:04:38 +1100

    RP-01 PipelineNode styles

--- src/components/ReactPipeline/shared/PipelineNode.tsx
+++ src/components/ReactPipeline/shared/PipelineNode.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { ReactPipesIntent } from "../types";
+import { ReactPipesIntent, ReactPipesSize } from "../types";
 import * as allIcons from "@tabler/icons-react";
 import { PipelineIcon } from "../icons";
+import "./PipelineNode.css"
 
 
 export interface NodeProps {
     intent: ReactPipesIntent,
     active: boolean,
-    icon: PipelineIcon
+    icon: PipelineIcon,
+    size: ReactPipesSize,
+    retries: number,
 }
 
 export const PipelineNode = (props: NodeProps) => {
@@ -15,8 +18,18 @@ export const PipelineNode = (props: NodeProps) => {
     const IconToBeUsed = allIcons[props.icon];
 
     return( 
-    <span className="pipeline-node">
-        <IconToBeUsed/>
+        <span className={"rp-node-wrapper" + ` ${props.size} ${props.intent} ${props.active ? "rp-state-active" : ""}`}>
+            <span className={"rp-node"}>
+                {
+                props.retries && 
+                    <span className={"rp-retries" + ` ${props.size} ${props.intent}`}>
+                        {props.retries}
+                    </span>
+                }
+                <span className={"rp-node-icon"}>
+                    <IconToBeUsed/>
+                </span>    
+            </span>
     </span>);
   };
   
--- src/components/ReactPipeline/types.ts
+++ src/components/ReactPipeline/types.ts
@@ -1,6 +1,15 @@
 export enum ReactPipesIntent {
-    PENDING,
-    SUCCESS,
-    FAILURE,
-    IN_PROGRESS
+    NONE = "rp-intent-none",
+    WARNING = "rp-intent-warning",
+    SUCCESS = "rp-intent-success",
+    FAILURE = "rp-intent-failure",
+    IN_PROGRESS = "rp-intent-in-progress"
+}
+
+export enum ReactPipesSize {
+    XS = "rp-size-xs",
+    S = "rp-size-s",
+    M = "rp-size-m",
+    L = "rp-size-l",
+    XL = "rp-size-xl"
 }
