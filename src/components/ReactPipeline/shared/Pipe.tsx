f7848a98d8e77f0353af08d03bb8b729e5f6e624
jamesgiu <james.giuffrida@gmail.com>
2023-11-07 22:17:30 +1100

    PP-02 Fix loading bar offset width

--- src/components/ReactPipeline/shared/Pipe.tsx
+++ src/components/ReactPipeline/shared/Pipe.tsx
@@ -12,7 +12,9 @@ export interface PipeProps {
 export const Pipe = (props: PipeProps) => {
     return (
         <div className={"rp-pipe-wrapper" + ` ${props.size} ${props.intent} ${props.active ? "rp-state-active" : ""}`}>
-            <div className={"rp-pipe"} style={{width: props.progressPercent + "%"}}/>
+            <div className={"rp-pipe-loading-wrapper"} style={{width: props.progressPercent + "%"}}>
+                <div className={"rp-pipe"}/>
+            </div>
         </div>
     );
   };
