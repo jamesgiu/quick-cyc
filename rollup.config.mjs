9c523300ec1084b51183f65d00c4b4f07f540db3
jamesgiu <james.giuffrida@gmail.com>
2023-11-16 01:04:05 +1100

    RP-XX Fix rollup deploy

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,8 +2,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
 
-const packageJson = require("./package.json");
+
+import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
@@ -24,11 +26,14 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      // NEW
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
